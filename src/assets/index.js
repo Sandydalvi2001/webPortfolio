49f2b29f55ff09af43d801bbf6735e1909415985
SANDESH DALVI <69284228+Sandydalvi2001@users.noreply.github.com>
2023-10-28 16:29:35 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,7 +8,7 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 
 import css from "./tech/css.png";
-import java from "./tech/java.png";
+import java from "./tech/Java.png";
 import cpp from "./tech/cpp.png";
 import python from "./tech/python.png";
 import sql from "./tech/sql.png";
